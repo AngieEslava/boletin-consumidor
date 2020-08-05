e3a73ba11a12015f128839749d17e6f5f8fa5d19
AngieEslava <egangiest@gmail.com>
2020-08-05 01:33:12 -0500

    correccion errores

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -6,9 +6,9 @@ const Inicio =()=>{
     return(
         <div>
             <p className="font-karla">Propuesta de logo identificador</p>
-            <img src={logo} />
+            <img src={logo} alt="logo" />
             <p className="font-karla">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, nemo molestias similique possimus fuga ad assumenda quae sequi voluptas nesciunt magnam aspernatur nisi quidem id quas voluptatum voluptate dolorum, officia est consequatur temporibus repellendus. Aut soluta sunt voluptatum, veritatis assumenda accusantium ex dicta! Debitis architecto dignissimos sit culpa doloremque soluta earum ad aliquid dolorum facere praesentium sapiente, enim, nostrum consectetur possimus quae exercitationem itaque. Quasi aliquam sit inventore odio quam suscipit deleniti. <br/>Dolor consectetur nisi debitis animi sit ad delectus quis sapiente odit omnis magnam quam, recusandae expedita veniam at nemo molestias tempora ratione accusamus, iste quaerat aliquam laborum? Quae totam consectetur, voluptate quibusdam autem nesciunt quos non! Atque vero ipsam quidem! Doloremque debitis velit necta! Magnam obcaecati dicta beatae!</p>
-            <img src={noticonsumidor} className="img-center" />
+            <img src={noticonsumidor} className="img-center" alt="noti"/>
             <h2 className="title-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae delectus deserunt totam nihil repudiandae eveniet eos magni explicabo ex facilis!</h2>
         </div>
     )
